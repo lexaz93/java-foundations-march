d02d53f54c4d8f4151dfee4108b153df74d9e204
lexaz93 <lexaz93@mail.ru>
2020-06-17 18:54:35 +0400

    Exceptions hw

---
+++ src/main/java/exceptions/hw_exeptions/HWExceptions.java
@@ -0,0 +1,28 @@
+package exceptions.hw_exeptions;
+
+public class HWExceptions {
+    public static void main(String[] args) {
+        try {
+            try {
+                throw new MyException("Моё исключение!");
+            } catch (MyException e) {
+                System.err.println(e.getMessage());
+                throw new MyException(e.getMessage());
+            } finally {
+                System.out.println("Блок кода который выполняется в любом случае!");
+            }
+        } catch (MyException e) {
+            System.err.println(e.getMessage() + " 2");
+        } finally {
+            System.out.println("Блок кода который выполняется в любом случае - 2!");
+            try {
+                Person alex = new Person("Alex", -1);
+            }
+            catch (MyRuntimeException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
+
+    }
+}
