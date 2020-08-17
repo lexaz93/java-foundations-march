a73f049adac6c79e3b66933692a8a84a148e152c
lexaz93 <lexaz93@mail.ru>
2020-08-17 22:16:10 +0400

    Extra HW collections

---
+++ src/main/java/streams/model/Student.java
@@ -0,0 +1,14 @@
+package streams.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Student {
+    private final String name;
+    private int courseNum;
+    private final Faculty faculty;
+
+
+}
