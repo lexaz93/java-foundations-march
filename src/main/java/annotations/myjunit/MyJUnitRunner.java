0364ce75bc57167cc19f1f6f050eea5bc26673bb
lexaz93 <lexaz93@mail.ru>
2020-08-29 20:32:36 +0400

    Testing with my own jUnit=)

--- src/main/java/annotations/myjunit/MyJUnitRunner.java
+++ src/main/java/annotations/myjunit/MyJUnitRunner.java
@@ -1,30 +1,70 @@
 package annotations.myjunit;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class MyJUnitRunner {
 
     public static void main(String[] args) throws ClassNotFoundException {
-        int countPassed = 0;
-        int countFailed = 0;
         SomethingTest test = new SomethingTest();
         final Class<?> testClazz = Class.forName("annotations.myjunit.SomethingTest");
-        for (Method method : testClazz.getMethods()) {
+
+        testing(testClazz, test, Before.class);
+
+        testingGeneral(testClazz, test);
+
+        testing(testClazz, test, After.class);
+
+        displayNameTesting(testClazz, test);
+
+
+
+    }
+
+    public static void testingGeneral(Class<?> testFromObject, SomethingTest test) {
+        for (Method method : testFromObject.getMethods()) {
             try {
-                if (method.isAnnotationPresent(Test.class)) {
-                    method.invoke(new SomethingTest());
-                    countPassed++;
+                if (method.isAnnotationPresent(Test.class) &&
+                        !method.isAnnotationPresent(Before.class) &&
+                        !method.isAnnotationPresent(After.class) &&
+                        !method.isAnnotationPresent(BeforeEach.class) &&
+                        !method.isAnnotationPresent(AfterEach.class)) {
+                    method.invoke(test);
+                    testing(testFromObject, test, BeforeEach.class);
+                    System.out.println(method.getName() + " успешно прошел");
+                    testing(testFromObject, test, AfterEach.class);
+                }
+            } catch (Throwable e) {
+                testing(testFromObject, test, BeforeEach.class);
+                System.err.println(method.getName() + " успешно упал");
+                testing(testFromObject, test, AfterEach.class);
+
+            }
+        }
+    }
+
+    public static void testing(Class<?> testFromObject, SomethingTest test, Class<? extends Annotation> annotationClass) {
+        for (Method method : testFromObject.getMethods()) {
+            try {
+                if (method.isAnnotationPresent(Test.class) && method.isAnnotationPresent(annotationClass)) {
+                    method.invoke(test);
                     System.out.println(method.getName() + " успешно прошел");
                 }
             } catch (Throwable e) {
-                countFailed++;
                 System.err.println(method.getName() + " успешно упал");
             }
         }
+    }
 
-        System.out.println("-------------------------------" +
-                "\nКоличество упавших тестов " + countFailed +
-                "\nКоличество пройденных тестов " + countPassed +
-                "\n-------------------------------");
+    public static void displayNameTesting(Class<?> testFromObject, SomethingTest test) {
+        for (Method method : testFromObject.getMethods()) {
+            try {
+                if (method.isAnnotationPresent(Test.class) && method.isAnnotationPresent(DisplayName.class)) {
+                    method.invoke(test);
+                    System.out.println(method.getAnnotation(DisplayName.class).string());
+                }
+            } catch (Throwable ignored) {
+            }
+        }
     }
 }
